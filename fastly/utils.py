ea8ad1342a4e68cd4764bb2ce5100b44c7ea60bc
Berker Peksag <berker.peksag@gmail.com>
2018-10-19 07:39:30 +0300

    Retry failed PURGE requests

--- fastly/utils.py
+++ fastly/utils.py
@@ -2,7 +2,27 @@ import urllib.parse
 
 import requests
 
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry as Urllib3Retry
+from requests.packages.urllib3.exceptions import ConnectTimeoutError
+
 from django.conf import settings
+
+
+class Retry(Urllib3Retry):
+
+    def _is_connection_error(self, err):
+        return isinstance(
+            err,
+            (
+                # This is the default exception in the original implementation
+                # of this method. It's a subclass of
+                # urllib3.exceptions.HTTPError.
+                ConnectTimeoutError,
+                # PYDOTORG-STAGING-1N, PYDOTORG-STAGING-1P, PYDOTORG-STAGING-1P
+                requests.ConnectionError,
+            ),
+        )
 
 
 def purge_urls(*paths):
@@ -13,8 +33,28 @@ def purge_urls(*paths):
     if not api_key or settings.DEBUG:
         return
 
-    with requests.session() as session:
+    # TODO: We can extract this logic out in the future.
+    session = requests.session()
+    retry = Retry(
+        total=3,
+        backoff_factor=0.3,
+        status_forcelist=(
+            500,  # Internal Server Error
+            502,  # Bad Gateway
+            503,  # Service Unavailable
+            504,  # Gateway Timeout
+        ),
+        # Default value for 'method_whitelist' doesn't include
+        # PURGE since it's a non-standard verb.
+        method_whitelist=(
+            'PURGE',
+        ),
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('https://', adapter)
+    with session:
         session.headers['Fastly-Key'] = api_key
         for path in paths:
             url = urllib.parse.urljoin('https://www.python.org', path)
+            # TODO: We should probably catch MaxRetryError and log it.
             session.request('PURGE', url)
